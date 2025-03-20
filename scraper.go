408b2c837014be5238f27186433ce472a7d9b57d
phillipashe <ashephillip@gmail.com>
2025-03-20 16:32:42 -0400

    imported colly

--- scraper.go
+++ scraper.go
@@ -2,9 +2,14 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/gocolly/colly"
 )
 
 func main() {
 	// scraping logic goes here
+	_ = colly.NewCollector(
+		colly.AllowedDomains("www.scrapingcourse.com"),
+	)
 	fmt.Println("this works")
 }
