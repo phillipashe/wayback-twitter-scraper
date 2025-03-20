4650534a67fb12c226f5cbe7e6871079ea86dc00
phillipashe <ashephillip@gmail.com>
2025-03-20 18:24:08 -0400

    switch to using config file, connect to initial json response

--- scraper.go
+++ scraper.go
@@ -1,9 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
-	"flag"
 	"fmt"
+	"os"
 
 	"github.com/gocolly/colly"
 )
@@ -13,18 +14,64 @@ func HandleTweet(*colly.Collector) {
 }
 
 func main() {
-	// scraping logic goes here
+
 	c := colly.NewCollector(
-		colly.AllowedDomains("www.changethislater.com"),
+		colly.AllowedDomains("archive.org", "web.archive.org"),
 	)
+	c.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36"
 
 	var initial_url string
-	flag.StringVar(&initial_url, "initial_url", "", "Initial URL to scrape")
-	flag.Parse()
+	file, err := os.ReadFile("settings.json")
+	if err != nil {
+		panic(err)
+	}
+	var data map[string]interface{}
+	if err := json.Unmarshal(file, &data); err != nil {
+		panic(err)
+	}
+	initial_url = data["initial_url"].(string)
 
 	if initial_url == "" {
 		panic(errors.New("initial_url is required"))
 	}
 
-	fmt.Printf("initial_url: %s", initial_url)
+	// // called before an HTTP request is triggered
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Requesting initial JSON of URLs")
+	})
+
+	// // triggered when the scraper encounters an error
+	c.OnError(func(_ *colly.Response, err error) {
+		fmt.Println("Something went wrong: ", err)
+	})
+
+	// fired when the server responds
+	c.OnResponse(func(r *colly.Response) {
+		fmt.Println("Page visited: ", r.Request.URL)
+		var resp [][]string
+		var urls []string
+
+		if err := json.Unmarshal(r.Body, &resp); err != nil {
+			panic(err)
+		}
+		for _, ov := range resp {
+			for _, iv := range ov {
+				urls = append(urls, iv)
+			}
+		}
+
+		// use urls
+		for _, url := range urls {
+			fmt.Println(url)
+		}
+	})
+
+	err = c.Visit(initial_url)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	c.Wait()
+
+	// fmt.Printf("initial_url: %s", initial_url)
 }
