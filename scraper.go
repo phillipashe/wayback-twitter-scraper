7e5e1c06909fdd440c6e8c7471b599fd65cca298
phillipashe <ashephillip@gmail.com>
2025-03-20 17:07:25 -0400

    pass initial wayback url via flag

--- scraper.go
+++ scraper.go
@@ -1,15 +1,30 @@
 package main
 
 import (
+	"errors"
+	"flag"
 	"fmt"
 
 	"github.com/gocolly/colly"
 )
 
+func HandleTweet(*colly.Collector) {
+	fmt.Println("Tweet")
+}
+
 func main() {
 	// scraping logic goes here
-	_ = colly.NewCollector(
-		colly.AllowedDomains("www.scrapingcourse.com"),
+	c := colly.NewCollector(
+		colly.AllowedDomains("www.changethislater.com"),
 	)
-	fmt.Println("this works")
+
+	var initial_url string
+	flag.StringVar(&initial_url, "initial_url", "", "Initial URL to scrape")
+	flag.Parse()
+
+	if initial_url == "" {
+		panic(errors.New("initial_url is required"))
+	}
+
+	fmt.Printf("initial_url: %s", initial_url)
 }
